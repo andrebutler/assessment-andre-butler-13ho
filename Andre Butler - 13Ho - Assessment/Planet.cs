e59a36b0840dfb0f9f0a76391bb34feececd6a82
Andre Butler <andrebutler@mmc.school.nz>
2020-08-11 11:57:02 +1200

    Image now appears in the game panel, although the image size is too small and it does not spawn in the correct place, therefore, this will need to be amended.

---
+++ Andre Butler - 13Ho - Assessment/Planet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace Andre_Butler___13Ho___Assessment
+{
+    class Planet
+    {
+
+        // declare fields to use in the class
+        public int x, y, width, height;//variables for the rectangle
+        public Image planetImage;//variable for the planet's image
+        public Rectangle planetRec;//variable for a rectangle to place our image in
+        public int score;
+        //Create a constructor (initialises the values of the fields)
+        public Planet()
+        {
+            x = 10;
+            y = 10;
+            width = 20;
+            height = 20;
+            //planetImage contains the plane1.png image
+            planetImage = Properties.Resources.planet;
+            planetRec = new Rectangle(x, y, width, height);
+        }
+
+
+        // Methods for the Planet class
+        public void DrawPlanet(Graphics g)
+        {
+            g.DrawImage(planetImage, planetRec);
+        }
+
+    }
+
+
+
+}
